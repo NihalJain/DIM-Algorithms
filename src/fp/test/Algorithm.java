eb9553b259eec38d629c4a1d562beadfcf2d9f39
Nihal Jain <nj.allzzwell@gmail.com>
2016-12-02 13:29:46 +0530

    Restructured project

---
+++ src/fp/test/Algorithm.java
@@ -0,0 +1,128 @@
+package fp.test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import fp.algorithm.frequentitemset.AlgoFPORed;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author nihal jain
+ * @version 1.0
+ */
+public class Algorithm {
+
+    /**
+     * Hash Map for Frequent itemset to its Support mapping.
+     */
+    public static Map<String, Float> frequent_list = new HashMap<>();
+    /**
+     * ArrayList for storing frequent ORed itemsets.
+     */
+    public static ArrayList<Integer[]> frequent_list_set = new ArrayList<>();
+
+    /**
+     * main function
+     *
+     * @param arg
+     * @throws FileNotFoundException If input file not found
+     * @throws IOException throws IOException if any.
+     */
+    public static void main(String[] arg) throws FileNotFoundException, IOException {
+        String input = null;
+        Float minsupp = null;
+        int parsed = 0;
+        Scanner sc = new Scanner(System.in);
+
+        // ------------Input file --------------
+        System.out.print("Enter Input file path : ");
+        try {
+            input = fileToPath(sc.nextLine()); //use this for IDE(testing)
+        } catch (Exception e) {
+            System.err.println("error: Input file not found.");
+            System.exit(-1);
+        }
+        
+        // ------------Support --------------
+        System.out.print("Enter Support Threshold : ");
+        try {
+            minsupp = sc.nextFloat();
+        } catch (Exception e) {
+            System.err.println("error: Invalid input for minimum Support.");
+            System.exit(-1);
+        }
+
+        // preprocess dataset
+        long parseStart = System.currentTimeMillis();
+        ParseDataset parse = new ParseDataset();
+        input = parse.ParseData(input);
+        parsed = 1;
+        long parseEnd = System.currentTimeMillis();
+
+        System.out.println();
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println("                      DIM Bit Set Based ALGORITHM");
+        System.out.println("=======================================================================");
+        System.out.println("                     MINING WITH BELOW PARAMETERS ");
+        System.out.println("=======================================================================");
+        System.out.println("DataSet : " + input);
+        System.out.println("Support Threshold : " + minsupp);
+        System.out.println("-----------------------------------------------------------------------");
+
+        // PHASE 1: finding All frequent ORed itemsets
+        long lStartTime = System.currentTimeMillis();
+
+        AlgoFPORed fpgrowth = new AlgoFPORed();
+        fpgrowth.runAlgorithm(input, minsupp);
+        int databaseSize = fpgrowth.getDatabaseSize();
+
+        long lMidTime = System.currentTimeMillis();
+
+        // PHASE 2: Generating all rules from the set of frequent itemsets
+        /*AlgoORedRules rules = new AlgoORedRules();
+        rules.Generate_Rules(minconf, parsed);*/
+        long lEndTime = System.currentTimeMillis();
+
+        // summarizing results
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("DATABASE SIZE " + databaseSize + " Total items : " + AlgoFPORed.total_singles);
+        System.out.println("Elapsed milliseconds (Preprocessing): " + (parseEnd - parseStart));
+        System.out.println("Elapsed milliseconds ( ORed Itemsets): " + (lMidTime - lStartTime));
+        System.out.println("Elapsed milliseconds (Rules + ORed itemsets): " + (lEndTime - lStartTime));
+        System.out.println("Elapsed milliseconds (preprocessing + Rules + ORed itemsets): " + (lEndTime - parseStart));
+
+        // linux command for getting memory utiliation of this process
+        try {
+            String s;
+            Process p = Runtime.getRuntime().exec(" ps -C java -O rss");
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while ((s = br.readLine()) != null) {
+                System.out.println(s);
+            }
+
+        } catch (Exception ex) {
+            System.out.println("unable to get memory Utilization.");
+        }
+        System.out.println("======================================================================\n\n");
+    }
+
+    /**
+     * Method to find complete path of input file.
+     *
+     * @param filename input file name given by user.
+     * @return complete input path to dataset.
+     * @throws UnsupportedEncodingException Exception if file encoding is not
+     * supported.
+     */
+    public static String fileToPath(String filename) throws UnsupportedEncodingException {
+        URL url = Algorithm.class.getResource(filename);
+        return java.net.URLDecoder.decode(url.getPath(), "UTF-8");
+    }
+}
---
+++ src/fp/test/ParseDataset.java
@@ -0,0 +1,117 @@
+package fp.test;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+/**
+ * This class preprocesses the dataset. It reads the input dataset and creates a
+ * new dataset with items represented by an integer ID.
+ *
+ * @author nihal jain
+ * @version 1.0
+ */
+public class ParseDataset {
+
+    /**
+     * HashMap for storing Mapping of assigned integer ID to item
+     */
+    public static Map<Integer, String> map = new HashMap<>();
+
+    /**
+     * Method to Preprocess dataset.
+     *
+     * @param input the path to an input file containing a transaction database.
+     * @return New input path to dataset.
+     */
+    public String ParseData(String input) {
+        // Mapping of item to assigned integer ID
+        Map<String, Integer> mapitem = new HashMap<>();
+        // BufferedReader object to read transations of input file.
+        BufferedReader reader = null;
+        // PrintWriter object to write mapped transaction to new input file.
+        PrintWriter writer = null;
+        try {
+            reader = new BufferedReader(new FileReader(input));
+        } catch (Exception e) {
+            System.err.println("error: Unable to open input file");
+            System.exit(-1); // exit
+        }
+
+        // Creating new input file
+        int len = input.length();
+        int i = len - 1;
+        for (; i > len - 7; i--) {
+            if (input.charAt(i) == '.') { // extension checking
+                break;
+            }
+        }
+        if (i == len - 7) { // if no extension provided
+            i = len;
+        }
+        String out = (String) input.subSequence(0, i);
+        out += "_int.txt";
+        System.out.println("Mapped Input file: " + out);
+        try {
+            writer = new PrintWriter(out, "UTF-8");
+
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            System.err.println("error: Unable to open item to integer ID mapping file");
+            System.exit(-1); // exit
+        }
+        String line;
+        Integer count = 0; // integer ID
+
+        try {
+            // for each line (transaction) until the end of the file
+            while (((line = reader.readLine()) != null)) {
+                // if the line is a comment, is empty or is a kind of metadata
+                if (line.isEmpty() == true || line.charAt(0) == '#' || line.charAt(0) == '%' || line.charAt(0) == '@') {
+                    continue;
+                }
+                // splitting transaction using delimeter space or tab
+                StringTokenizer lineSplited = new StringTokenizer(line);
+                // new transaction to be written in file
+                String transaction = "";
+                // for each item in the input transaction
+                while (lineSplited.hasMoreElements()) {
+                    Integer temp = null;
+                    String s = lineSplited.nextToken();
+                    // checking wheather item is mapped with integer ID or not
+                    if ((temp = mapitem.get(s)) == null) {
+                        // assign integer ID to item
+                        mapitem.put(s, count);
+                        // reverse mapping for final output
+                        map.put(count, s);
+                        temp = count;
+                        count++;
+                    }
+                    // adding integer ID correspond to intem
+                    transaction = transaction + temp + " ";
+                }
+                if (transaction != "") {
+                    // writing to new input file
+                    writer.println(transaction);
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("error: Unable to read transaction from input file");
+        }
+        try {
+            reader.close();
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("error: Unable to close input or output file");
+        }
+	
+        // System.out.println(mapitem);
+
+        // returning new input path
+        return out;
+    }
+}
